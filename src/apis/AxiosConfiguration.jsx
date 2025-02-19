c0a932d7c3b8bc753cba47072bbcebdeee8a8a1c
AnPhan <126398348+AnPhanCNTT-K63@users.noreply.github.com>
2025-02-19 23:29:25 +0700

    fix: api url

--- src/apis/AxiosConfiguration.jsx
+++ src/apis/AxiosConfiguration.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3000/api";
+const API_URL =
+  "https://web-production-52e1.up.railway.app/api" ||
+  "http://localhost:3000/api";
 
 const apiClient = axios.create({
   baseURL: API_URL,
