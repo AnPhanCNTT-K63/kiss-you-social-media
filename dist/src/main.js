3e7c8cdf39abdb2b904481aa60a2db0576b41755
AnPhan <126398348+AnPhanCNTT-K63@users.noreply.github.com>
2025-02-19 21:34:13 +0700

    fix: env

--- dist/src/main.js
+++ dist/src/main.js
@@ -28,6 +28,7 @@ async function bootstrap() {
     }));
     app.useGlobalInterceptors(new transform_interceptor_1.TransformInterceptor());
     await app.listen(process.env.PORT ?? 3000);
+    console.log(process.env.PORT);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
